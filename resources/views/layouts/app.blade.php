e3ad15bfca4e73f3039fdb47ec6ebb92c44e7ceb
darman-prog <damezago24@gmail.com>
2025-10-16 12:06:58 -0500

    Ajuste para preparar el proyecto para laravel ui auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,20 +1,18 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     @yield('css')
 </head>
 <body>
-
 
     @include('layouts.header')
 
     @yield('content')
 
     @include('layouts.footer')
-
 
 </body>
 </html>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,13 +1,16 @@
 <!-- ========== NAVBAR ========== -->
-    <nav class="navbar">
-        <div class="navbar-container">
-            <a href="/" class="navbar-brand">Mi Tienda</a>
-            <ul class="navbar-menu">
-                <li><a href="/">Inicio</a></li>
-                <li><a href="/products">Productos</a></li>
-                <li><a href="/categories">Categorías</a></li>
-                <li><a href="/about">Acerca de</a></li>
-                <li><a href="/contact">Contacto</a></li>
-            </ul>
-        </div>
-    </nav>
+<nav class="navbar">
+    <div class="navbar-container">
+        <a href="/" class="navbar-brand">
+            <img src="https://unab.edu.co/wp-content/uploads/2020/03/Img_2368.png" alt="UNAB Logo">
+            Mi Tienda UNAB
+        </a>
+        <ul class="navbar-menu">
+            <li><a href="/">Inicio</a></li>
+            <li><a href="/products">Productos</a></li>
+            <li><a href="/categories">Categorías</a></li>
+            <li><a href="/about">Acerca de</a></li>
+            <li><a href="/contact">Contacto</a></li>
+        </ul>
+    </div>
+</nav>
